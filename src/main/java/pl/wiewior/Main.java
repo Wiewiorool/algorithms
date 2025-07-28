6f25e6b19a05c64a9f423b3dc2cec57d28586fd1
Wiewiorool <wasinskipatryk@gmail.com>
2025-07-28 19:49:51 +0200

    Add Bubble sort

--- src/main/java/pl/wiewior/Main.java
+++ src/main/java/pl/wiewior/Main.java
@@ -6,7 +6,21 @@ public class Main {
         int[] sorted = SelectionSort.sort(tab);
 
         for (int num : sorted) {
-            System.out.println(num + " ");
+            System.out.print(num + ", ");
+        }
+        System.out.println();
+        int[] tab1 = {1, 2, 5, 4, 10, 10, -1, 5};
+        int[] tab2 = {-5, 7, -2, -5, -100, 100, 52, 150};
+        int[] tab3 = {5, 0, 0, 0, 1, -1};
+        int[] tab4 = {0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0};
+        int[] sortedTab1 = SelectionSort.bubbleSort(tab1);
+        int[] sortedTab2 = SelectionSort.bubbleSort(tab2);
+        int[] sortedTab3 = SelectionSort.bubbleSort(tab3);
+        int[] sortedTab4 = SelectionSort.bubbleSort(tab4);
+        System.out.println("Bubble: " );
+        for (int num : sortedTab4) {
+            System.out.print(+ num + ", ");
         }
     }
+
 }
--- src/main/java/pl/wiewior/SelectionSort.java
+++ src/main/java/pl/wiewior/SelectionSort.java
@@ -19,6 +19,18 @@ public class SelectionSort {
         return tab;
     }
 
+    public static int[] bubbleSort(int[] tab) {
+        for (int i = 0; i < tab.length - 1; i++) {
+            for (int j = 0; j < tab.length - 1 - i; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    swap(tab, j, j + 1);
+
+                }
+            }
+        }
+        return tab;
+    }
+
     private static void swap(int[] tab, int i, int j) {
         int temp = tab[i];
         tab[i] = tab[j];
