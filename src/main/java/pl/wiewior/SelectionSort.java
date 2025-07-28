f51e7c2ff4d3bfb8806de7ab4f4d2dc81a846aee
Wiewiorool <wasinskipatryk@gmail.com>
2025-07-28 19:36:52 +0200

    fixed sort method

--- src/main/java/pl/wiewior/SelectionSort.java
+++ src/main/java/pl/wiewior/SelectionSort.java
@@ -4,14 +4,17 @@ public class SelectionSort {
 
     public static int[] sort(int[] tab) {
 
-        for (int i = 0; i < tab.length; i++) {
+        for (int i = 0; i < tab.length - 1; i++) { //ostatni element nie musi być już przetwarzany
             int min = Integer.MAX_VALUE;
+            int minIndex = 1;
             for (int j = i; j < tab.length; j++) {
                 if (min > tab[j]) {
                     min = tab[j];
+                    minIndex = j;
                 }
             }
-            swap(tab, i, min);
+            swap(tab, i, minIndex);
+
         }
         return tab;
     }
