68ad17f868e4ca0a8674afa85b3c4210ff72ab9a
Wiewior <wasisnkipatryk@gmail.com>
2025-07-28 19:36:52 +0200

    fixed sort method

--- src/main/java/pl/wiewior/Main.java
+++ src/main/java/pl/wiewior/Main.java
@@ -2,6 +2,11 @@ package pl.wiewior;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] tab = {4, 2, 3, 55, 62, 13, 45, 999};
+        int[] sorted = SelectionSort.sort(tab);
+
+        for (int num : sorted) {
+            System.out.println(num + " ");
+        }
     }
 }
