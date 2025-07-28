6ca4d7fb73bd63c0595b50dc421ffd390fa7ac6a
Wiewiorool <wasinskipatryk@gmail.com>
2025-07-28 08:10:25 +0200

    add swap method

--- src/main/java/pl/wiewior/SelectionSort.java
+++ src/main/java/pl/wiewior/SelectionSort.java
@@ -1,2 +1,25 @@
-package pl.wiewior;public class SelectionSort {
+package pl.wiewior;
+
+public class SelectionSort {
+
+    public static int[] sort(int[] tab) {
+
+        for (int i = 0; i < tab.length; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = i; j < tab.length; j++) {
+                if (min > tab[j]) {
+                    min = tab[j];
+                }
+            }
+            swap(tab, i, min);
+        }
+        return tab;
+    }
+
+    private static void swap(int[] tab, int i, int j) {
+        int temp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = temp;
+
+    }
 }
