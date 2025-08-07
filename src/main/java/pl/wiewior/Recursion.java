64bb3492341a93ccad12464f4a9fd7baa7cd8674
Wiewiorool <wasinskipatryk@gmail.com>
2025-08-07 21:58:05 +0200

    Add recursion homework 1 and 2

--- src/main/java/pl/wiewior/Recursion.java
+++ src/main/java/pl/wiewior/Recursion.java
@@ -1,10 +1,13 @@
 package pl.wiewior;
 
+import java.util.Arrays;
+
 public class Recursion {
 
     public static void main(String[] args) {
         int[] tab = {123, 3, 4, 5, 6, 7};
-        printRecursion(tab, 0);
+        homeWork();
+
     }
 
     public static void printRecursion(int[] tab, int i) {
@@ -19,6 +22,19 @@ public class Recursion {
         for (int i = 0; i < tab.length; i++) {
             System.out.println(tab[i]);
         }
+
+    }
+
+    public static void homeWork() {
+        int sum = 0;
+        int[] array = RandomTab.generateRandomTab(50);
+        System.out.println(Arrays.toString(array));
+
+        for (int i = 0; i < 10; i++) {
+            int result = array[i];
+            sum = sum + result;
+        }
+        System.out.println("Suma tablicy intów o długości 10: " + sum);
 
     }
 }
