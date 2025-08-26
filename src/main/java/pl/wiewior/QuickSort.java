d271be1ce360f38c76a5c7fe03077ad8f3f849d9
Wiewior <wasisnkipatryk@gmail.com>
2025-08-26 22:10:35 +0200

    Added second version of QuickSort Recursion

--- src/main/java/pl/wiewior/QuickSort.java
+++ src/main/java/pl/wiewior/QuickSort.java
@@ -23,4 +23,33 @@ public class QuickSort {
         Swap.swap(tab, i + 1, high);
         return i + 1;
     }
+    public static void recQuickSort(int[] nums, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int s = low;
+        int e = high;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
+
+        while (s <= e) {
+
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        recQuickSort(nums,low,e);
+        recQuickSort(nums,s,high);
+
+    }
 }
