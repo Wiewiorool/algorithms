fc5c2dd5768f08d877959e20828000e5c3e8b6a1
Wiewior <wasisnkipatryk@gmail.com>
2025-08-26 22:43:23 +0200

    Added second version of MergeSort Recursion

--- src/main/java/pl/wiewior/MergeSort.java
+++ src/main/java/pl/wiewior/MergeSort.java
@@ -44,4 +44,47 @@ public class MergeSort {
         return merge(left, right);
 
     }
+
+    public static int[] recMergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
+        }
+        int mid = arr.length / 2;
+
+        int[] left = recMergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = recMergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return mergeTwo(left, right);
+    }
+
+    private static int[] mergeTwo(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while ((i < first.length && j < second.length)) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
+        }
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+        return mix;
+
+    }
 }
